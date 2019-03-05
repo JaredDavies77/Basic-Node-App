18a99cffcaaac9e995de4849ec9149a8f4782385
Jared Davies <jdaviesgolf@pga.com>
2019-03-05 01:40:16 -0500

    Basic Node App

---
+++ main.js
@@ -0,0 +1,21 @@
+const greetings = require("./greeting.js");
+const number = require("./number.js");
+// "Hello"
+greetings.greetingEnglish();
+// "One"
+number.oneEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+// "Ichi"
+number.oneJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+// "Uno"
+number.oneSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+// "Einn"
+number.oneIcelandic();
---
+++ number.js
@@ -0,0 +1,15 @@
+exports.oneEnglish = function(){
+    console.log("One");
+   }
+   
+exports.oneJapanese = function(){
+    console.log("Ichi");
+   }
+
+exports.oneSpanish = function(){
+    console.log("Uno");
+   }
+
+exports.oneIcelandic = function(){
+    console.log("Einn");
+   }
